4012c5682b44c30853fa24096e5e73af558490d5
Anton Alekseev <Anton Alekseev>
2017-07-03 15:23:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (request, response)
+{
+    response.send('All right');
+});
+
+app.get('/kek', function (request, response)
+{
+    response.send('kek');
+});
+
+
+app.listen(app.get('port'), function ()
+{
+    console.log('Node app is running on port', app.get('port'));
+});
+
+//app.listen(3000);
+
