433dde1b1cc34f3e7ad94603a12fbe513a3bcdf4
Anton Alekseev <Anton Alekseev>
2017-07-04 14:01:59 +0300

    done get and check methods

--- index.js
+++ index.js
@@ -1,29 +1,66 @@
 var express = require('express');
 var app = express();
-
+var qs = require('qs');
+var get = require('simple-get');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (request, response)
-{
-    response.send('All right');
-});
+app.get('/',
+    function (request, response)
+    {
+        response.send('All right');
+    }
+);
 
-app.get('/kek', function (request, response)
-{
-    response.send('kek');
-});
+app.get('/kek',
+    function (request, response)
+    {
+        response.send('kek');
+    }
+);
 
-app.get('/receipts/check', function (request, response)
-{
-    response.send(request.query.toString());
-});
+app.get('/receipts/get',
+    function (request, response)
+    {
+        var params = request.query;
+        var params_string = qs.stringify(params);
 
+        get.concat("http://brand.cash/v1/receipts/get?" + params_string,
+            function (err, res, data)
+            {
+                if (err)
+                    throw err;
 
-app.listen(app.get('port'), function ()
-{
-    console.log('Node app is running on port', app.get('port'));
-});
+                response.send(data.toString());
+            }
+        );
 
-//app.listen(3000);
+    }
+);
+
+app.get('/receipts/check',
+    function (request, response)
+    {
+        var params = request.query;
+        var params_string = qs.stringify(params);
+
+        get.concat("http://brand.cash/v1/receipts/check?" + params_string,
+            function (err, res, data)
+            {
+                if (err)
+                    throw err;
+
+                response.send(data.toString());
+            }
+        );
+
+    }
+);
+
+app.listen(app.get('port'),
+    function ()
+    {
+        console.log('Node app is running on port', app.get('port'));
+    }
+);
 
