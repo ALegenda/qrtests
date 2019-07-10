e2446e314bfe92f1f1fd5e56d5c6bad059064320
TomKuper <adalekseev@edu.hse.ru>
2019-07-10 19:27:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
-var qs = require('qs');
-var get = require('simple-get');
-var url = "mongodb://TomKuper:dbpassword1234@ds137650.mlab.com:37650/heroku_t9zfwvj6";
 var mongodb = require("mongodb");
+var url = "mongodb://TomKuper:dbpassword1234@ds137650.mlab.com:37650/heroku_t9zfwvj6";
 var localBase;
-var ObjectID = require('mongodb').ObjectID;
-//
+
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
+
 
 mongodb.MongoClient.connect(
     process.env.MONGODB_URI || url,
@@ -27,159 +25,24 @@ mongodb.MongoClient.connect(
     }
 );
 
-app.get(
-    '/admin',
-    function (request, response)
-    {
-        response.render("index.html");
-    }
-);
 
 app.get(
     '/',
     function (request, response)
     {
-        response.send('All right');
-    }
-);
+        var collection = localBase.collection('tmp');
 
-app.get(
-    '/kek',
-    function (request, response)
-    {
-        response.send("kek");
-    }
-);
+        collection.find({}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
 
-app.get(
-    '/api/gethistory/:token',
-    function (request, response)
-    {
-        var collection = localBase.collection('Logs');
-        var token = request.params.token;
+            count = result[0].count;
 
-        collection.find({'token': token}).toArray(
-            function (err, results)
-            {
-                response.send(results); // output all records
-            }
-        );
+            collection.updateOne({"data":"tmp"},{$set: {"data" : "tmp", "count": ++count}});
 
-    }
-);
+            response.json("Проверяем нагрузоустойчивость бесплатного хостнига. На сайт зашли столько раз : "+count);
+        });
 
-app.get(
-    '/api/gethistory',
-    function (request, response)
-    {
-        var collection = localBase.collection('Logs');
-
-        collection.find({}).toArray(
-            function (err, results)
-            {
-                response.send(results); // output all records
-            }
-        );
-
-    }
-);
-
-app.get(
-    '/api/register',
-    function (request, response)
-    {
-        var collection = localBase.collection('users');
-    }
-);
-
-app.post(
-    '/receipts/get',
-    function (request, response)
-    {
-        var params = request.query;
-        var params_string = qs.stringify(params);
-        var collection = localBase.collection('Logs');
-
-        get.concat(
-            "http://brand.cash/v1/receipts/get?" + params_string,
-            function (err, res, data)
-            {
-                if (err)
-                    return response.status(500).send({error: 'Something failed!'});
-
-                if (!data.toString())
-                    return response.status(400).send({error: 'Bad params'});
-
-                var token = request.header("token");
-                if (token)
-                {
-                    var tmp = JSON.parse(data.toString());
-
-                    collection.findOne({'data': tmp}).then(
-                        function (doc)
-                        {
-                            if (!doc)
-                                collection.insertOne({"token": token, "data": tmp});
-                        });
-                }
-                else
-                    return response.status(400).send({error: 'Expected header token'});
-
-                response.send(data.toString());
-
-            }
-        );
-
-    }
-);
-
-app.get(
-    '/receipts/get',
-    function (request, response)
-    {
-        var params = request.query;
-        var params_string = qs.stringify(params);
-        var collection = localBase.collection('Logs');
-
-        get.concat(
-            "http://brand.cash/v1/receipts/get?" + params_string,
-            function (err, res, data)
-            {
-                if (err)
-                    return response.status(500).send({error: 'Something failed!'});
-
-                if (!data.toString())
-                    return response.status(400).send({error: 'Bad params'});
-
-                var tmp = JSON.parse(data.toString());
-
-                collection.insertOne({"token": "log", "data": tmp});
-
-                response.send(data.toString());
-
-            }
-        );
-
-    }
-);
-
-app.get(
-    '/receipts/check',
-    function (request, response)
-    {
-
-        var params = request.query;
-        var params_string = qs.stringify(params);
-
-        get.concat(
-            "http://brand.cash/v1/receipts/check?" + params_string,
-            function (err, res, data)
-            {
-                if (err) return response.status(500).send({error: 'Something failed!'});
-
-                response.send(data.toString());
-            }
-        );
 
     }
 );
